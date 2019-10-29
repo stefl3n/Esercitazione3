04ffc7fcdd3e66f1d3e9e7df3b2ffe7a56eb4fcc
MarcoTurzi <56305514+MarcoTurzi@users.noreply.github.com>
2019-10-29 17:09:53 +0100

    Aggiunto getchar() per catturare carattere invio dopo scanf

--- Project/SenzaConnessione/Client.c
+++ Project/SenzaConnessione/Client.c
@@ -18,8 +18,8 @@
 
 	struct hostent *host;
 	struct sockaddr_in clientaddr, servaddr;
-	int port, sd;
-	char * req;
+	int port, sd,byteletti;
+	char  req[10];
 	char c = 's';
 
 	if (argc != 3){
@@ -37,15 +37,24 @@
 	servaddr.sin_family = AF_INET;
 	host = gethostbyname(argv[1]);
 	port = atoi(argv[2]);
+	printf("%i \n", port);
 	if(port < 1024 || port > 65535){
 		printf("Errore porta"),
 		exit(1);
 	}
 
-	servaddr.sin_addr.s_addr = ((struct in_addr *)(host->h_addr))->s_addr;
-	servaddr.sin_port = htons(port);
+	if(host == NULL){
+		exit(2);
 
-	sd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	}else{
+
+		servaddr.sin_addr.s_addr = ((struct in_addr *)(host->h_addr))->s_addr;
+			servaddr.sin_port = htons(port);
+
+			sd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	}
 	if(sd < 0){
 		printf("Errore socket");
 		exit(1);
@@ -57,7 +66,8 @@
 		exit(1);
 	}
 
-	req = (char*)malloc(100*sizeof(char));
+
+	printf("%i \n",sizeof(req));
 
 	while(c == 's'){
 
@@ -65,20 +75,24 @@
 
 		printf("Inserisci il file che vuoi analizzare \n");
 		scanf("%s", req);
+		printf("%s",req);
+		getchar();
+		printf("%i \n",sizeof(req));
 		if(strcmp(req," ") == 0){
 
 			printf("Nome file non può essere vuoto \n");
 
 		}else {
 			len = sizeof(servaddr);
-			sendto(sd, req, sizeof(req), 0, (struct sockaddr *)&servaddr, len);
-			printf("Nome inviato\n");
+			byteletti = sendto(sd, &req, sizeof(req), 0, (struct sockaddr *)&servaddr, len);
+			printf("Nome inviato\n e byte inviati: %i \n", byteletti);
 			recvfrom(sd, &temp, sizeof(int), 0, (struct sockaddr *)&servaddr, &len);
 			printf("risposta ricevuta \n");
 			risposta = ntohl(temp);
 			printf("La parola piu lunga misura %d lettere \n", risposta);
 			printf("Vuoi continuare ? s/n \n");
 			scanf("%c", &c);
+
 
 		}
 
