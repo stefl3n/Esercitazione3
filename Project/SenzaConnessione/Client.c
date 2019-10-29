8c164c5a7b3fbd0e7da689328c28bfa5037b0c29
MarcoTurzi <56305514+MarcoTurzi@users.noreply.github.com>
2019-10-29 13:07:27 +0100

    Client ( non testato con server )

---
+++ Project/SenzaConnessione/Client.c
@@ -0,0 +1,90 @@
+/*
+ * Client.c
+ *
+ *  Created on: Oct 28, 2019
+ *      Author: root
+ */
+#include <unistd.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <string.h>
+
+
+	int main(int argc, char **argv){
+
+	struct hostent *host;
+	struct sockaddr_in clientaddr, servaddr;
+	int port, sd;
+	char * req;
+	char c = 's';
+
+	if (argc != 3){
+
+		printf("Errore argomenti");
+		exit(1);
+	}
+
+	memset((char *)&clientaddr, 0, sizeof(struct sockaddr_in));
+	clientaddr.sin_family = AF_INET;
+	clientaddr.sin_addr.s_addr = INADDR_ANY;
+	clientaddr.sin_port = 0;
+
+	memset((char*)&servaddr, 0 , sizeof(struct sockaddr_in));
+	servaddr.sin_family = AF_INET;
+	host = gethostbyname(argv[1]);
+	port = atoi(argv[2]);
+	if(port < 1024 || port > 65535){
+		printf("Errore porta"),
+		exit(1);
+	}
+
+	servaddr.sin_addr.s_addr = ((struct in_addr *)(host->h_addr))->s_addr;
+	servaddr.sin_port = htons(port);
+
+	sd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(sd < 0){
+		printf("Errore socket");
+		exit(1);
+	}
+
+	if(bind(sd,(struct sockaddr *) &clientaddr, sizeof(clientaddr)) < 0){
+
+		printf("Errore bind");
+		exit(1);
+	}
+
+	req = (char*)malloc(100*sizeof(char));
+
+	while(c == 's'){
+
+		int risposta, temp,len;
+
+		printf("Inserisci il file che vuoi analizzare \n");
+		scanf("%s", req);
+		if(strcmp(req," ") == 0){
+
+			printf("Nome file non può essere vuoto \n");
+
+		}else {
+			len = sizeof(servaddr);
+			sendto(sd, req, sizeof(req), 0, (struct sockaddr *)&servaddr, len);
+			printf("Nome inviato\n");
+			recvfrom(sd, &temp, sizeof(int), 0, (struct sockaddr *)&servaddr, &len);
+			printf("risposta ricevuta \n");
+			risposta = ntohl(temp);
+			printf("La parola piu lunga misura %d lettere \n", risposta);
+			printf("Vuoi continuare ? s/n \n");
+			scanf("%c", &c);
+
+		}
+
+
+
+	}
+	close(sd);
+	return 0;
+}
